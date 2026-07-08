ae7cc31f4f6331429c7778b0599ab85343b6e9ee
bongna7777023-commits <bongna7777023@gmail.com>
2026-07-08 20:30:06 +0700

    [feature #75] fix api and logic in classroom voting

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function user(Request $request): JsonResponse
+    {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PollController;
 use App\Http\Controllers\Api\VoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+
     Route::get('polls/active', [PollController::class, 'active'])->withoutMiddleware('auth:sanctum');
     Route::get('polls/{poll}/results', [PollController::class, 'results']);
 
